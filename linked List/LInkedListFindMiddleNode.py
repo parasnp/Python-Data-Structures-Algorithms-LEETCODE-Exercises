c7c965bdb88f8d12af053bafcf72dc2adb29b8ed
Paras .np {GLD} <68340735+parasnp@users.noreply.github.com>
2024-02-03 10:40:42 +0530

    Add files via upload

---
+++ linked List/LInkedListFindMiddleNode.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+    
+    def find_middle_node(self):
+        fast=self.head
+        slow=self.head
+        while(fast!=None and fast.next!=None):
+            fast=fast.next.next
+            slow=slow.next
+        return slow
+        
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value)
